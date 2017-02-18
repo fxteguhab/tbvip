f27e2b04e746635169a32634d0dec84c2b4c80d8
aaelsldehoc <aaelsldehoc@gmail.com>
2017-02-18 19:14:52 +0700

    Add discout field on product template

--- __openerp__.py
+++ __openerp__.py
@@ -23,6 +23,7 @@
 		'data/tbvip_data.xml',
 		'views/employee_view.xml',
 		'views/purchase_view.xml',
+		'views/product_view.xml',
 		'views/tbvip.xml',
 		'menu/tbvip_menu.xml',
 	],
--- product.py
+++ product.py
@@ -28,6 +28,7 @@ class product_template(osv.osv):
 	
 	_columns = {
 		'codex_id': fields.integer('MySQL Product ID'),
+		'discount': fields.char('Discount'),
 	}
 	
 	def create(self, cr, uid, vals, context={}):
