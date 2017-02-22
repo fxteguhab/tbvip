773864e561bd4bc81b46c570f9877aaa599540bd
aaelsldehoc <aaelsldehoc@gmail.com>
2017-02-22 11:24:57 +0700

    Add context on purchase order and remove direct_receive on create of PO

--- purchase.py
+++ purchase.py
@@ -42,19 +42,19 @@ class purchase_order(osv.osv):
 	# langsung confirm purchasenya bila diinginkan. otomatis dia bikin satu invoice dan satu incoming goods
 		if context.get('direct_confirm', False):
 			purchase_data = self.browse(cr, uid, new_id)
-			self.signal_workflow(cr, uid, [new_id], 'purchase_confirm')
+			self.signal_workflow(cr, uid, [new_id], 'purchase_confirm', context)
 		# langsung validate juga invoicenya
 			invoice_obj = self.pool.get('account.invoice')
 			for invoice in purchase_data.invoice_ids:
 				invoice_obj.write(cr, uid, [invoice.id], {
 					'date_invoice': purchase_data.date_order,
 				})
-				invoice_obj.signal_workflow(cr, uid, [invoice.id], 'invoice_open')
+				invoice_obj.signal_workflow(cr, uid, [invoice.id], 'invoice_open', context)
 	# langsung terima juga barangnya
-		if context.get('direct_receive', False):
-			purchase_data = self.browse(cr, uid, new_id)
-			receive_obj = self.pool.get('stock.picking')
-			picking_ids = [picking.id for picking in purchase_data.picking_ids]
-			receive_obj.action_done(cr, uid, picking_ids)
+	# 	if context.get('direct_receive', False):
+	# 		purchase_data = self.browse(cr, uid, new_id)
+	# 		receive_obj = self.pool.get('stock.picking')
+	# 		picking_ids = [picking.id for picking in purchase_data.picking_ids]
+	# 		receive_obj.action_done(cr, uid, picking_ids)
 		return new_id
 
