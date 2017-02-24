378649df73cfd0f44ebc03afa367b084867b1ed9
aaelsldehoc <aaelsldehoc@gmail.com>
2017-02-24 17:21:58 +0700

    Add purchase order line price changed notification

--- purchase.py
+++ purchase.py
@@ -1,3 +1,4 @@
+from openerp import SUPERUSER_ID
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 from datetime import datetime, date, timedelta
@@ -55,3 +56,43 @@ class purchase_order(osv.osv):
 				invoice_obj.signal_workflow(cr, uid, [invoice.id], 'invoice_open', context)
 		return new_id
 
+# ==========================================================================================================================
+
+
+class purchase_order_line(osv.osv):
+
+	_inherit = 'purchase.order.line'
+
+	def _message_cost_price_changed(self, cr, uid, data, product, order_id, context):
+		if product.standard_price > 0 and data['price_unit'] != product.standard_price:
+			purchase_order_obj = self.pool.get('purchase.order')
+			purchase_order = purchase_order_obj.browse(cr, uid, order_id)
+			# message post to SUPERUSER and all users in group Purchases Manager
+			group_obj = self.pool.get('res.groups')
+			purchase_manager_group_ids = group_obj.search(cr, uid,
+														  [('category_id.name', '=', 'Purchases'),
+														   ('name', '=', 'Manager')])
+			partner_ids = [SUPERUSER_ID]
+			for user in group_obj.browse(cr, uid, purchase_manager_group_ids).users:
+				partner_ids += [user.partner_id.id]
+			partner_ids = list(set(partner_ids))
+			purchase_order_obj.message_post(cr, uid, purchase_order.id, context=context, partner_ids=partner_ids,
+											body=_("There is a change on cost price for %s in %s Purchase Order")
+												 % (product.name, purchase_order.name))
+
+	def create(self, cr, uid, data, context=None):
+		new_order_line = super(purchase_order_line, self).create(cr, uid, data, context)
+		if 'product_id' in data and data['product_id'] and 'price_unit' in data and data['price_unit']:
+			product_obj = self.pool.get('product.product')
+			product = product_obj.browse(cr, uid, data['product_id'])
+			self._message_cost_price_changed(cr, uid, data, product, data['order_id'], context)
+		return new_order_line
+
+	def write(self, cr, uid, ids, data, context=None):
+		edited_order_line = super(purchase_order_line,self).write(cr, uid, ids, data, context)
+		for id in ids:
+			if 'price_unit' in data and data['price_unit']:
+				product_obj = self.pool.get('product.product')
+				product = product_obj.browse(cr, uid, self.browse(cr, uid, id).product_id.id)
+				self._message_cost_price_changed(cr, uid, data, product, self.browse(cr, uid, id).order_id.id, context)
+		return edited_order_line
