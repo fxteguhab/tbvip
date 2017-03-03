ca4309d187b7a1a13afcf52e46dad3f14a2f69da
Devi Handevi <devihandevi@yahoo.com>
2017-03-03 16:23:52 +0700

    [TL-174] Account Invoice Line discount amount dan override discount

--- account_invoice.py
+++ account_invoice.py
@@ -1,4 +1,5 @@
-from openerp import models, fields
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
 
 # ==========================================================================================================================
 
@@ -21,8 +22,13 @@ class account_invoice_line(models.Model):
 	_inherit = 'account.invoice.line'
 	
 	# METHODS ---------------------------------------------------------------------------------------------------------------
+	@api.depends('price_unit', 'discount_amount')
+	def _compute_discount_amount_percentage(self):
+		self.discount = (self.discount_amount / self.price_unit) * 100.0
 
 	# COLUMNS ---------------------------------------------------------------------------------------------------------------
 	discount_amount = fields.Float(string='Discount Amount')
+	discount = fields.Float(string='Discount (%)', digits= dp.get_precision('Discount'),
+							compute=_compute_discount_amount_percentage, default=0.0)
 
 	# OVERRIDES -------------------------------------------------------------------------------------------------------------
