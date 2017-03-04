70c853ed267b6a9337b103bceccc21b0a1e8edce
Devi Handevi <devihandevi@yahoo.com>
2017-03-04 21:05:10 +0700

    [TL-177] Add comment saja

--- account_invoice.py
+++ account_invoice.py
@@ -26,6 +26,11 @@ class account_invoice_line(models.Model):
 	# METHODS ---------------------------------------------------------------------------------------------------------------
 	@api.depends('price_unit', 'discount_amount')
 	def _compute_discount_amount_percentage(self):
+		"""
+		Final price for each line is computed with the equation (self.price_unit*(1-(self.discount or 0.0)/100.0)).
+		To make use the discount field, the discount_amount will be converted into percentage, so that the subtotal
+		of each line will be updated as expected when there's a change in the field.
+		"""
 		if(self.price_unit != 0):
 			self.discount = (self.discount_amount / self.price_unit) * 100.0
 		else:
