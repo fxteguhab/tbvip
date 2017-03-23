56e77852da9c9740964aab25a5136dfc33c3423a
sammyh27 <sammyh27@gmail.com>
2017-03-24 00:14:09 +0700

    tiket 5, needs 12

--- product.py
+++ product.py
@@ -22,10 +22,29 @@ class product_category(osv.osv):
 class product_template(osv.osv):
 	_inherit = 'product.template'
 	
+	# FIELD FUNCTION METHODS ------------------------------------------------------------------------------------------------
+	
+	def _purchase_order_line_ids(self, cr, uid, ids, field_name, arg, context={}):
+		result = {}
+		for data in self.browse(cr, uid, ids):
+			order_line_obj = self.pool.get('purchase.order.line')
+			order_line_ids = order_line_obj.search(cr, uid, [('product_id', '=', data.id)], order='date_planned desc',)
+			supplier_ids = []
+			unique_supplier_order_line_ids = []
+			for order_line_id in order_line_ids:
+				order_line = order_line_obj.browse(cr, uid, order_line_id)
+				if order_line.partner_id.id not in supplier_ids:
+					supplier_ids.append(order_line.partner_id.id)
+					unique_supplier_order_line_ids.append(order_line_id)
+			result[data.id] = unique_supplier_order_line_ids
+		return result
+	
 	# COLUMNS ---------------------------------------------------------------------------------------------------------------
 	
 	_columns = {
 		'codex_id': fields.integer('MySQL Product ID'),
+		'purchase_order_line_ids': fields.function(_purchase_order_line_ids, method=True, type="one2many",
+												   string="Last Purchase", relation="purchase.order.line"),
 	}
 	
 	def create(self, cr, uid, vals, context={}):
