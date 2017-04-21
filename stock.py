7e738b6f014671e3fa13699228819626cc0c8483
sammyh27 <sammyh27@gmail.com>
2017-04-21 15:30:35 +0700

    stock bonus

--- stock.py
+++ stock.py
@@ -48,14 +48,30 @@ class stock_bonus_usage(osv.osv):
 	
 	def _usage_line_min(self, cr, uid, ids, context=None):
 		# Cek bonus usage line harus ada minimal 1 baris
-		for bonus_usages in self.browse(cr, uid, ids, context):
-			for bonus_usage in bonus_usages:
-				if len(bonus_usage.bonus_usage_line) == 0:
+		for bonus_usage in self.browse(cr, uid, ids, context):
+			if len(bonus_usage.bonus_usage_line) == 0:
+				return False
+		return True
+	
+	def _move_from_min_qty(self, cr, uid, ids, context=None):
+		# Cek bahwa setiap product di line harus punya minimal sejumlah qty yang diminta di stock location move_from
+		quant_obj = self.pool["stock.quant"]
+		uom_obj = self.pool["product.uom"]
+		for bonus_usage in self.browse(cr, uid, ids, context):
+			for line in bonus_usage.bonus_usage_line:
+				domain = [('location_id', '=', bonus_usage.move_from.id), ('product_id','=', line.product_id.id)]
+				quant_ids = quant_obj.search(cr, uid, domain, context=context)
+				quants = quant_obj.browse(cr, uid, quant_ids, context=context)
+				tot_qty = sum([x.qty for x in quants])
+				if line.unit_of_measure and line.product_id.uom_id.id != line.unit_of_measure.id:
+					tot_qty = uom_obj._compute_qty_obj(cr, uid, line.product_id.uom_id, tot_qty, line.unit_of_measure, context=context)
+				if tot_qty < line.qty:
 					return False
 		return True
 	
 	_constraints = [
 		(_usage_line_min, _('You must have at least one usage line.'), ['bonus_usage_line']),
+		(_move_from_min_qty, _('The location does not have that many product.'), ['move_from']),
 	]
 	
 	# ACTION ----------------------------------------------------------------------------------------------------------------
