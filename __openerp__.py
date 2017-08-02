de51160d2aa2108f310260da510085a76677e809
Devi Handevi <devihandevi@gmail.com>
2017-08-02 15:28:10 +0700

    [Depends]

--- __openerp__.py
+++ __openerp__.py
@@ -10,10 +10,10 @@
 	'website': 'http://www.chjs.biz',
 	'depends': [
 		"base", "board", "web", "website",
-		"chjs_custom_view",
+		"chjs_custom_view", "chjs_price_list",
 		"account_accountant", "account_cancel", "stock", "sale", "purchase", "fleet", "hr", "hr_attendance","hr_expense",
 		"purchase_needs", "purchase_sale_discount", "stock_opname", "account_receivable_limit", 
-		"sale_direct_cash", "canvassing", "hr_employee_point", 
+		"sale_direct_cash", "canvassing", "hr_employee_point", "product_custom_conversion", "product_production"
 	],
 	'sequence': 150,
 	'data': [
