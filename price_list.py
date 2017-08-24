1ffafb244ea868dc07d06b3ee8f25ee05ee3e978
aaelsldehoc <aaelsldehoc@gmail.com>
2017-08-24 22:42:59 +0700

    Remove price list onchange domain overrides as it overwrite domain from views

--- price_list.py
+++ price_list.py
@@ -5,30 +5,6 @@ from openerp.tools.translate import _
 
 import utility
 
-# ==========================================================================================================================
-class price_list_line_product(osv.osv):
-	_inherit = 'price.list.line.product'
-	
-# OVERRIDE ------------------------------------------------------------------------------------------------------------------
-	
-	def onchange_product_template_id(self, cr, uid, ids, product_template_id, context=None):
-		res = super(price_list_line_product, self).onchange_product_template_id(cr, uid, ids, product_template_id, context)
-		res = utility.update_uom_domain_price_list(res)
-		return res
-
-# ==========================================================================================================================
-
-class price_list_line_category(osv.osv):
-	_inherit = 'price.list.line.category'
-
-# OVERRIDE ------------------------------------------------------------------------------------------------------------------
-	def onchange_product_category_id(self, cr, uid, ids, product_category_id, context=None):
-		res = super(price_list_line_category, self).onchange_product_category_id(cr, uid, ids, product_category_id, context)
-		res = utility.update_uom_domain_price_list(res)
-		return res
-
-# ==========================================================================================================================
-
 class product_current_price(osv.osv):
 	_inherit = 'product.current.price'
 	
