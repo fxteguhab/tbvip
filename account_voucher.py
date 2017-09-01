8b7fb4d1d9ac7088523a8a5a41c5bb228dabc8e9
Devi Handevi <devihandevi@gmail.com>
2017-09-01 10:27:24 +0700

    [KONTRA BON] account voucher shows bank_id, domain from partner_id, onchange

--- account_voucher.py
+++ account_voucher.py
@@ -7,6 +7,22 @@ from openerp.osv import osv, fields
 class account_voucher(osv.osv):
 	_inherit = 'account.voucher'
 	
+	# OVERRIDES -------------------------------------------------------------------------------------------------------------
+	
+	def onchange_partner_id(self, cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context=None):
+		result = super(account_voucher, self).onchange_partner_id(
+			cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context)
+		
+		partner_obj = self.pool.get('res.partner')
+		partner = partner_obj.browse(cr, uid, [partner_id])
+		if not result.get('domain', False):
+			result['domain'] = {}
+		result['domain'].update({
+			'bank_id': [('id', 'in', partner.bank_ids.ids)],
+		})
+		
+		return result
+	
 	# FIELD FUNCTION METHODS ------------------------------------------------------------------------------------------------
 	
 	def _total_amount_unreconciled(self, cr, uid, ids, field_name, arg, context=None):
@@ -39,5 +55,6 @@ class account_voucher(osv.osv):
 			type='float', store=True),
 		'check_maturity_date': fields.date(string='Check Maturity Date',
 			readonly=True, states={'draft': [('readonly', False)]}),
-		'bank_id': fields.function(_bank_ids, string="Bank Account", type='many2one', relation="res.partner.bank"),
+		# 'bank_id': fields.function(_bank_ids, string="Bank Account", type='many2one', relation="res.partner.bank", readonly=False),
+		'bank_id': fields.many2one('res.partner.bank', 'Bank Account'),
 	}
