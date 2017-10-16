1df7dafaad5bc32f01b5d047da9fb0af44d1a9fa
Devi Handevi <devihandevi@gmail.com>
2017-10-16 16:39:29 +0700

    [KOREKSI BON] count total paid for account invoice refund

--- koreksi_bon.py
+++ koreksi_bon.py
@@ -93,7 +93,7 @@ class koreksi_bon(osv.osv_memory):
 		for koreksi_bon in self.browse(cr, uid, ids):
 			# copy SO lines
 			order_lines = []
-			for order_line in koreksi_bon.sale_order_id.order_line:
+			for order_line in koreksi_bon.order_line:
 				order_lines.append([0, False,{
 					'name': order_line.name,
 					'product_id': order_line.product_id.id,
@@ -106,6 +106,10 @@ class koreksi_bon(osv.osv_memory):
 				}])
 			
 			sale_order = sale_order_obj.browse(cr, uid, koreksi_bon.sale_order_id.id)
+			total_paid = sale_order.payment_transfer_amount + \
+						 sale_order.payment_cash_amount + \
+						 sale_order.payment_receivable_amount + \
+						 sale_order.payment_giro_amount
 			# cancel invoice
 			# invoice cannot be canceled if paid, instead ???????????????
 			account_invoice_cancel_obj.invoice_cancel(cr, uid, sale_order.invoice_ids.ids)
