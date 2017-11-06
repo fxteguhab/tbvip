2f07e582afbf3a4b791f0e5aff303eaf3c3e77d8
Devi Handevi <devihandevi@gmail.com>
2017-11-06 08:54:25 +0700

    [VOUCHER] remove unused field

--- account_voucher.py
+++ account_voucher.py
@@ -25,18 +25,6 @@ class account_voucher(osv.osv):
 	
 	# FIELD FUNCTION METHODS ------------------------------------------------------------------------------------------------
 	
-	def _total_amount_unreconciled(self, cr, uid, ids, field_name, arg, context=None):
-		result = {}
-		account_voucher_line_obj = self.pool.get('account.voucher.line')
-		for record in self.browse(cr, uid, ids, context=context):
-			total_amount_unreconciled = 0
-			account_voucher_line_ids = account_voucher_line_obj.search(cr, uid, [('voucher_id', '=', record.id)])
-			for account_voucher_id in account_voucher_line_ids:
-				account_voucher_line = account_voucher_line_obj.browse(cr, uid, [account_voucher_id])
-				total_amount_unreconciled += account_voucher_line.amount_unreconciled
-			result[record.id] = total_amount_unreconciled
-		return result
-	
 	def _bank_ids(self, cr, uid, ids, field_name, arg, context=None):
 		result = {}
 		for account_voucher_data in self.browse(cr, uid, ids, context=context):
@@ -51,8 +39,6 @@ class account_voucher(osv.osv):
 	# COLUMNS ---------------------------------------------------------------------------------------------------------------
 	
 	_columns = {
-		'total_amount_unreconciled': fields.function(_total_amount_unreconciled, string="Total Amount Reconciled",
-			type='float', store=True),
 		'check_maturity_date': fields.date(string='Check Maturity Date',
 			readonly=True, states={'draft': [('readonly', False)]}),
 		# 'bank_id': fields.function(_bank_ids, string="Bank Account", type='many2one', relation="res.partner.bank", readonly=False),
