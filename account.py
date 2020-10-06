7acd751c7ca0e142088fd2938c471256fe38e9bc
fxteguhab <31503256+fxteguhab@users.noreply.github.com>
2020-10-06 11:49:44 +0700

    add supp_invoice_number to account.report

--- account.py
+++ account.py
@@ -718,9 +718,13 @@ class account_account(osv.osv):
 class account_invoice_report(osv.osv):
 	_inherit = "account.invoice.report"
 
+	_columns = {
+		'supplier_invoice_number' : fields.char("Supplier Invoice Number"),
+	}
+
 	def _select(self):
 		select_str = """
-			SELECT sub.id, sub.date, sub.product_id, sub.partner_id, sub.country_id,
+			SELECT sub.id, sub.date, sub.product_id, sub.partner_id, sub.country_id, sub.supplier_invoice_number,
 				sub.payment_term, sub.period_id, sub.uom_name, sub.currency_id, sub.journal_id,
 				sub.fiscal_position, sub.user_id, sub.company_id, sub.nbr, sub.type, sub.state,
 				sub.categ_id, sub.date_due, sub.account_id, sub.account_line_id, sub.partner_bank_id,
@@ -728,3 +732,57 @@ class account_invoice_report(osv.osv):
 				cr.rate as currency_rate, sub.residual as residual, sub.commercial_partner_id as commercial_partner_id
 		"""
 		return select_str
+
+	def _sub_select(self):
+		select_str = """
+				SELECT min(ail.id) AS id,
+					ai.date_invoice AS date,
+					ail.product_id, ai.partner_id, ai.payment_term, ai.period_id, ai.supplier_invoice_number as supplier_invoice_number,
+					u2.name AS uom_name,
+					ai.currency_id, ai.journal_id, ai.fiscal_position, ai.user_id, ai.company_id,
+					count(ail.*) AS nbr,
+					ai.type, ai.state, pt.categ_id, ai.date_due, ai.account_id, ail.account_id AS account_line_id,
+					ai.partner_bank_id,
+					SUM(CASE
+						 WHEN ai.type::text = ANY (ARRAY['out_refund'::character varying::text, 'in_invoice'::character varying::text])
+							THEN (- ail.quantity) / u.factor * u2.factor
+							ELSE ail.quantity / u.factor * u2.factor
+						END) AS product_qty,
+					SUM(CASE
+						 WHEN ai.type::text = ANY (ARRAY['out_refund'::character varying::text, 'in_invoice'::character varying::text])
+							THEN - ail.price_subtotal
+							ELSE ail.price_subtotal
+						END) AS price_total,
+					CASE
+					 WHEN ai.type::text = ANY (ARRAY['out_refund'::character varying::text, 'in_invoice'::character varying::text])
+						THEN SUM(- ail.price_subtotal)
+						ELSE SUM(ail.price_subtotal)
+					END / CASE
+						   WHEN SUM(ail.quantity / u.factor * u2.factor) <> 0::numeric
+							   THEN CASE
+									 WHEN ai.type::text = ANY (ARRAY['out_refund'::character varying::text, 'in_invoice'::character varying::text])
+										THEN SUM((- ail.quantity) / u.factor * u2.factor)
+										ELSE SUM(ail.quantity / u.factor * u2.factor)
+									END
+							   ELSE 1::numeric
+						  END AS price_average,
+					CASE
+					 WHEN ai.type::text = ANY (ARRAY['out_refund'::character varying::text, 'in_invoice'::character varying::text])
+						THEN - ai.residual
+						ELSE ai.residual
+					END / (SELECT count(*) FROM account_invoice_line l where invoice_id = ai.id) *
+					count(*) AS residual,
+					ai.commercial_partner_id as commercial_partner_id,
+					partner.country_id
+		"""
+		return select_str
+
+	def _group_by(self):
+		group_by_str = """
+				GROUP BY ail.product_id, ai.date_invoice, ai.id,
+					ai.partner_id, ai.payment_term, ai.supplier_invoice_number, ai.period_id, u2.name, u2.id, ai.currency_id, ai.journal_id,
+					ai.fiscal_position, ai.user_id, ai.company_id, ai.type, ai.state, pt.categ_id,
+					ai.date_due, ai.account_id, ail.account_id, ai.partner_bank_id, ai.residual,
+					ai.amount_total, ai.commercial_partner_id, partner.country_id
+		"""
+		return group_by_str
